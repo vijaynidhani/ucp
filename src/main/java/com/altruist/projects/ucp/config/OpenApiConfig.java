20810e0e5818abf55df7207a59053d3354bfa221
vijay dhiman <vijaypal.dhiman@altruistindia.com>
2026-02-08 19:25:03 +0400

    added

---
+++ src/main/java/com/altruist/projects/ucp/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.altruist.projects.ucp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("UCP Payment API")
+                        .version("1.0.0")
+                        .description("Unified Payment Controller API - A comprehensive payment processing system supporting multiple payment gateways")
+                        .contact(new Contact()
+                                .name("Altruist Projects")
+                                .email("support@altruist.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
