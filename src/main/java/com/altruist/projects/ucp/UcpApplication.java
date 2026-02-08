1983ff8a21f11ba6469d37d2de195a2de12aa280
vijay dhiman <vijaypal.dhiman@altruistindia.com>
2026-02-08 16:25:20 +0400

    Initial commit - UCP project

---
+++ src/main/java/com/altruist/projects/ucp/UcpApplication.java
@@ -0,0 +1,13 @@
+package com.altruist.projects.ucp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altruist/projects/ucp/UcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altruist.projects.ucp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
