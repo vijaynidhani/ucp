ee8e4eb006467db218e353da0d78c4cf613f66d9
vijay dhiman <vijaypal.dhiman@altruistindia.com>
2026-02-08 16:33:06 +0400

    Add SLF4J with Logback logging support

--- src/main/java/com/altruist/projects/ucp/HelloController.java
+++ src/main/java/com/altruist/projects/ucp/HelloController.java
@@ -3,11 +3,16 @@ package com.altruist.projects.ucp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HelloController {
 
 	@GetMapping("/hello")
 	public String hello() {
+		log.info("Hello endpoint called");
+		log.debug("Processing hello request");
 		return "Hello, World!";
 	}
 
